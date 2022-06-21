4013e0b57efbd86cfe90908fedffcccfcee09cb3
agrinkevich <grinkevich.alina@gmail.com>
2022-06-21 21:39:42 +0600

    add a test that checks if a product page contains 'add to cart' button

---
+++ test_items.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+def test_add_to_cart_button_is_present(browser):
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+
+    add_to_cart_btn = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "btn-add-to-basket"))
+     )
+
+    assert add_to_cart_btn is not None, "The button is missing"
